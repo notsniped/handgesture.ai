07ee28f7c9005ea35c75fee0be54621006f9a111
SniperBlaze <72265661+shoddyDEV@users.noreply.github.com>
2021-09-26 12:41:16 +0530

    Completed JavaScript.
    Added displayresult functions and unicode emojis.

--- main.js
+++ main.js
@@ -1,3 +1,4 @@
+prediction = "";
 Webcam.set({
     width:350,
     height:300,
@@ -12,7 +13,33 @@ function take_snapshot() {
     });
 }
 console.log("ml5 Version: ", ml5.version);
-classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/fOAmoy_uX/model.json", modalLoaded);
+classifier = ml5.imageClassifier("https://teachablemachine.withgoogle.com/models/wl03AFCK4/model.json", modalLoaded);
 function modalLoaded() {
     console.log("Modal Loaded Successfully!");
+}
+function check() {
+    img = document.getElementById("captured_image");
+    classifier.classify(img, gotResult);
+}
+function gotResult(error, results) {
+    if(error) {
+        console.error(error);
+    }
+    else {
+        console.log(results);
+        document.getElementById("result_gesture_name").innerHTML = results[0].label;
+        prediction = results[0].label;
+        if(results[0].label == "Amazing!") {
+            document.getElementById("result_gesture_name").innerHTML = "This is amazing!";
+            document.getElementById("update_emoji").innerHTML = "&#128076;";
+        }
+        if(results[0].label == "Best!") {
+            document.getElementById("result_gesture_name").innerHTML = "This is the best!";
+            document.getElementById("update_emoji").innerHTML = "&#128077;";
+        }
+        if(results[0].label == "Victory!") {
+            document.getElementById("result_gesture_name").innerHTML = "I have declared victory!";
+            document.getElementById("update_emoji").innerHTML = "&#9996;";
+        }
+    }
 }
